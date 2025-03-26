b559ff5e56447d28b464e8b99ffff72cc7f8de81
EC2 Default User <ec2-user@ip-172-31-12-221.ap-northeast-1.compute.internal>
2025-03-26 04:20:58 +0000

    devise名前ログイン機能実装

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+    root_path
   end
 
   def after_sign_out_path_for(resource)
@@ -46,7 +47,11 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
