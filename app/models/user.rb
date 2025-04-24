014762924d1c66b6e74d941348308c3e1a951b14
EC2 Default User <ec2-user@ip-172-31-12-221.ap-northeast-1.compute.internal>
2025-04-24 08:22:01 +0000

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :groups, through: :memberships
   has_many :expenses, foreign_key: :payer_id
 
+  validates :name, presence: true
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
