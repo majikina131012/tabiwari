1ee49fef6d40b639f4889f9100aca8e0ff6f8707
EC2 Default User <ec2-user@ip-172-31-12-221.ap-northeast-1.compute.internal>
2025-05-12 12:07:03 +0000

    空欄入力時のエラー画面の実装

--- app/controllers/public/expenses_controller.rb
+++ app/controllers/public/expenses_controller.rb
@@ -3,11 +3,9 @@ class Public::ExpensesController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
     @expense = @group.expenses.new(expense_params)
-  
     if @expense.save
       user_ids = params[:expense][:user_ids].reject(&:blank?).map(&:to_i)
       share_amount = @expense.amount / user_ids.size.to_f
-  
       user_ids.each do |user_id|
         @expense.shares.create(
           user_id: user_id,
@@ -16,10 +14,9 @@ class Public::ExpensesController < ApplicationController
           pay_to_user_id: @expense.payer_id == user_id ? nil : @expense.payer_id
         )
       end
-  
-      redirect_to group_path(@group)
+      redirect_to group_path(@group.id)
     else
-      render :new
+      redirect_to error_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'public/homes#top'
   scope module: :public do
+    get 'error' => 'homes#error'
     resources :users, only: [:edit, :update]
     resources :groups, only: [:new, :create, :index, :show, :destroy] do
       resources :memberships, only: [:create, :destroy]
